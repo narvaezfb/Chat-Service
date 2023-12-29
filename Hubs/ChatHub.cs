fb9fc82e40a73ed178e5e93afd64653adeb34663
Fabian Narvaez <narvaezfb4@hotmail.com>
2023-12-28 21:09:00 -0500

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat_Service.Hubs
+{
+	public class ChatHub: Hub
+	{
+		//public override async Task OnConnectedAsync()
+		//{
+		//	await Clients.All.SendAsync("RecieveMessage", $"{Context.ConnectionId} has joined");
+		//}
+		public async Task SendMessage(string message)
+		{
+			await Clients.All.SendAsync("RecieveMessage", message);
+		}
+	}
+}
+
