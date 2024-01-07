26c26ad3389ca104443d1e77f19adbd8c48a158f
Fabian Narvaez <narvaezfb4@hotmail.com>
2024-01-06 23:24:09 -0500

    Adding messages controller

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Chat_Service.Models;
+using Chat_Service.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chat_Service.Controllers
+{
+    [Route("[controller]")]
+    public class MessagesController : ControllerBase
+    {
+        private readonly ChatServiceDbContext _context;
+
+        public MessagesController(ChatServiceDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetMessage/{messageId}")]
+        public async Task<IActionResult> GetOneMessage(string messageId)
+        {
+            try
+            {
+                var message = await _context.Messages.FindAsync(messageId);
+
+                if(message == null)
+                {
+                    return BadRequest("Not message found with that ID");
+                }
+
+                return Ok(message);
+
+            }catch(Exception e)
+            {
+                return StatusCode(500, $"Internal server error: {e.Message}");
+            }
+        }
+
+        [HttpGet("GetUsersConversation/{user1}/{user2}")]
+        public async Task<IActionResult> GetUsersChat(string user1, string user2)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(user1) || string.IsNullOrEmpty(user2))
+                {
+                    return BadRequest("Invalid arguments on the request");
+                }
+                
+                var messages = await _context.Messages.Where(m => (m.SenderId == user1 && m.ReceiverId == user2) ||
+                                                                  (m.SenderId == user2 && m.ReceiverId == user1))
+                                                      .OrderByDescending(m => m.Timestamp)
+                                                      .ToListAsync();
+
+                if(messages.Count <= 0)
+                {
+                    return BadRequest("Not messages found for these users");
+                }
+
+                return Ok(messages);
+
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500, $"Internal server error: {e.Message}");
+            }
+        }
+    }
+}
+
--- Migrations/20240107034644_InitialCreate.cs
+++ Migrations/20240107034644_InitialCreate.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Chat_Service.Migrations
 {
     /// <inheritdoc />
-    public partial class MessageEntity : Migration
+    public partial class InitialCreate : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -19,23 +19,39 @@ namespace Chat_Service.Migrations
                     SenderId = table.Column<string>(type: "text", nullable: false),
                     ReceiverId = table.Column<string>(type: "text", nullable: false),
                     Content = table.Column<string>(type: "text", nullable: false),
-                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                    Timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Messages", x => x.MessageId);
                 });
 
+            migrationBuilder.CreateTable(
+                name: "UserConnections",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "text", nullable: false),
+                    ConnectionId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserConnections", x => x.UserId);
+                });
+
             migrationBuilder.CreateIndex(
                 name: "IX_Messages_ReceiverId",
                 table: "Messages",
-                column: "ReceiverId",
-                unique: true);
+                column: "ReceiverId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Messages_SenderId",
                 table: "Messages",
-                column: "SenderId",
+                column: "SenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserConnections_ConnectionId",
+                table: "UserConnections",
+                column: "ConnectionId",
                 unique: true);
         }
 
@@ -44,6 +60,9 @@ namespace Chat_Service.Migrations
         {
             migrationBuilder.DropTable(
                 name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "UserConnections");
         }
     }
 }
